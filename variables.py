17aa9c3840d7edf2118fa3b37a76176f2d2ff6e8
kaivanov8 <kosiv6799@gmail.com>
2024-10-18 19:37:14 +0700

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,6 @@
+# ПЗ Переменные
+completed_h_w = 12
+number_of_hours = 1.5
+course_name = 'Python'
+average_time = number_of_hours / completed_h_w
+print ('Курс:', course_name, ', всего задач:', completed_h_w, ', затрачено часов:', number_of_hours, ', среднее время выполнения', average_time,'часа.')
